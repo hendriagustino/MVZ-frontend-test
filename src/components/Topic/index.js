49e83bc95d41ecece042a7e654a062072713158d
Hendri Agustino <hendriagustino@hendriagustino.local>
2019-05-04 16:26:58 +0800

    [Hendri] - trying out Upvoting & Downvoting functionality

--- src/components/Topic/index.js
+++ src/components/Topic/index.js
@@ -8,69 +8,82 @@ class index extends Component {
   }
 
   addTopic = (data, e) => {
-    
-    this.setState({ 
+    this.setState({
       topic: this.state.topic.concat({
-        'title':data.title,
+        'title': data.title,
         'text': data.text,
-        'vote': 18,
+        'vote': 77
       })
     });
   }
 
-render() {
-  
-  const result = this.state.topic.map(topic=>
-    ({
-      title: topic.title, text: topic.text, vote: topic.vote
-    })
-  );
+  upVote = (i) => {
+    var stateCopy = Object.assign({}, this.state);
+    stateCopy.topic[i].vote += 1;
+    this.setState(stateCopy);
+    console.log(stateCopy);
+  }
 
-  console.log(result);
+  downVote = (i) => {
+    var stateCopy = Object.assign({}, this.state);
+    stateCopy.topic[i].vote -= 1;
+    this.setState(stateCopy);
+  }
 
-  return (
-    <div>
-      <AddTopic addTopic={this.addTopic} />
-      <hr></hr>
+  render() {
+
+    const result = this.state.topic.map(topic => ({ title: topic.title, text: topic.text, vote: topic.vote }));
+    console.log(result);
+
+    return (
       <div>
-        {
-          this.state.topic.map((topic,i) => 
-            <div key={i} 
-              style= {{
-                width : '50vw', 
-                height : '200px', 
-                border: '1px solid black', 
-                marginBottom: '10px', 
-                borderRadius: '5px',
-                paddingLeft : '10px'
-              }}>
 
-              <h1>{topic.title}</h1>
-              <p>{topic.text}</p>
+        <AddTopic addTopic={this.addTopic} />
+        <hr></hr>
 
-              <button 
-                style={{fontSize: '20px'}} 
-                onClick={() => this.setState({ vote: this.state.topic.vote + 1 })}> 
-                + 
+        <div>
+          
+          {
+            this.state.topic.map((topic, i) =>
+              <div key={i}
+                style={{
+                  width: '50vw',
+                  height: '200px',
+                  border: '1px solid black',
+                  marginBottom: '10px',
+                  borderRadius: '5px',
+                  paddingLeft: '10px'
+                }}
+              >
+
+                <h1>{topic.title}</h1>
+                <p>{topic.text}</p>
+
+                <button
+                  style={{ fontSize: '20px' }}
+                  // onClick={() => this.setState({ topic: this.state.topic[i].vote + 1 })}>
+                  onClick={(i)=>this.upVote}>
+                  +
               </button>
 
-              <span style={{marginLeft:'10px'}}>{topic.vote} &nbsp;</span>
-              
-              <button 
-                style={{fontSize: '20px'}} 
-                onClick={()=>this.setState({ vote: this.state.topic.vote -1})}> 
-                - 
+                <span style={{ marginLeft: '10px' }}>{topic.vote} &nbsp;</span>
+
+                <button
+                  style={{ fontSize: '20px' }}
+                  // onClick={() => this.setState({ topic: this.state.topic[i].vote - 1 })}>
+                  onClick={(i)=>this.downVote}>
+                  -
               </button>
 
-            </div>
-          )
-        }
-        
+              </div>
+            )
+          }
+
+        </div>
+
       </div>
-      
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default index;
